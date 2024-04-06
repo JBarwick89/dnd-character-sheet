95bbfebed278ab36889895e20e3e5670e49af3b1
Josh Barwick <j.Barwick89@gmail.com>
2024-04-06 14:07:52 -0500

    update backend to use tsx. add types to requests. update react-router-dom version

--- backend/app.ts
+++ backend/app.ts
@@ -1,21 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
 const app = express();
 const port = "8000";
-require("dotenv").config();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGODBUSERNAME}:${process.env.MONGODBSECRET}@cluster0.4dfgnks.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/characters", async (req, res) => {
+app.get("/characters", async (req: Request, res: Response) => {
   try {
     const database = client.db("dnd_character_sheet");
     const charactersCollection = database.collection("characters");
@@ -32,11 +34,22 @@ app.get("/characters", async (req, res) => {
   }
 });
 
-app.post("/character", async (req, res) => {
+interface CharacterRequest extends Request {
+  body: {
+    name: string;
+    playerName: string;
+  };
+}
+
+app.post("/character", async (req: CharacterRequest, res: Response) => {
   try {
     const database = client.db("dnd_character_sheet");
     const charactersCollection = database.collection("characters");
-    const character = { name: req.body.name, playerName: req.body.playerName };
+
+    const character = {
+      name: req.body.name,
+      playerName: req.body.playerName,
+    };
     const insertResult = await charactersCollection.insertOne(character);
 
     console.log(
